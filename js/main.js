8b21bb5c2d0dfacf9be86b2dad6e883ac433d6b7
Andrea Ligorio <andrea85kro@gmail.com>
2021-03-17 18:27:18 +0100

    Seconda milestone completata

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-	/* Milestone 1
-	Partendo dalla struttura dati che troviamo sotto, mostriamo in pagina tutte le icone disponibili come da layout. */
+/* Milestone 1
+Partendo dalla struttura dati che troviamo sotto, mostriamo in pagina tutte le icone disponibili come da layout. */
 
 	const icons = [
 		{
@@ -104,20 +104,45 @@ $(document).ready(function () {
 
 	const iconsContainer = $('.icons');
 
-	iconsContainer.empty();
-
+	
+	/* Milestone 2
+	Coloriamo le icone per tipo */
+	
+	const color = ["blue", "orange", "purple"];
+	
+	const iconsType = [];
 	icons.forEach((element) => {
-
-		const { name, prefix, family } = element;
-
-		iconsContainer.append(`
-		<div>
-			<i class="${family} ${prefix}${name}"></i>
-			<div class="icons_title">${name}</div>
-		</div>`);
+		if (!iconsType.includes(element.type)) {
+			iconsType.push(element.type);
+		}
 	});
+	console.log(iconsType);
+	
+	const iconsColored = icons.map((element) => {
+		const indexType = iconsType.indexOf(element.type);
+		if(indexType != -1) {
+			element.color = color[indexType];
+		}
+		return element;
+	});
+	console.log(iconsColored);
 
-
+	printIcons(iconsColored, iconsContainer);
 });
 
 /* FUNZIONI */
+
+function printIcons(array, container) {
+	container.empty();
+
+	array.forEach((element) => {
+
+		const { name, prefix, family, color } = element;
+
+		container.append(`
+		<div>
+			<i class="${family} ${prefix}${name}" style="color: ${color}"></i>
+			<div class="icons_title" style="color: ${color}">${name}</div>
+		</div>`);
+	});
+}
